445753bc70a289b64fbf0df18f3e0cb5b279636e
Daniel Mateos <daniel@mateos.io>
2021-03-25 11:07:33 +0100

    Command history from class of 2021-03-25

---
+++ command_history_20210325.sh
@@ -0,0 +1,49 @@
+clear
+git config
+git config --global --get-all
+git config --get-all
+git config --get-all user
+git config --get-all user.name
+git config --get-all user.email
+git config --global user.name "Daniel Mateos"
+git config --global user.email "daniel@mateos.io"
+ls
+cd repos
+mkdir newfolder
+cd newfolder
+git init
+pwd
+git add README.md
+c
+git status
+git commit -m 'Initial commit'
+git status
+git diff
+clear
+git status
+git show HEAD
+cd ../pyspark-training
+clear
+git show
+clear
+glog
+git show cad97bf
+clear
+git log
+git log --branches --graph --decorate --oneline
+cd ../ThinkStats2
+glog
+cd ../newfolder
+glog
+clear
+glog
+git diff
+cd ..
+cd newfolder
+git diff
+git add README.md
+git status
+git diff --cached
+git commit -m 'Add grocery list'
+less ~/.history
+tail -n 100 ~/.history > command_history_20210325
