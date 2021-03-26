0205b53535fc360ab8834b2e5e08352da9be80b0
Daniel Mateos <daniel@mateos.io>
2021-03-26 11:07:19 +0100

    History for second day

---
+++ 06-command_history_20210326.sh
@@ -0,0 +1,189 @@
+git status
+ls
+ll 
+ll -a
+git log --branches --graph -- decorate --oneline
+git log --branches --graph --decorate --oneline
+git show 9208de6
+c
+cd ../newfolder
+glog
+ls
+git status
+git diff
+git stash 
+git stash pop
+glog
+clear
+glog
+git checkout 69b214c
+ls
+cat README.md
+clear
+glgo
+glog
+git show 5ce0fad
+git checkout 5ce0fad
+git stash 
+git checkout 5ce0fad
+clear
+cat README.md
+glog
+git checkout master
+glog
+cat README.md
+git stash pop
+git diff
+clear
+git status
+git diff
+git add README.md
+git status
+git diff --cached
+git commit -m 'Add shopping list'
+glog
+git reset HEAD --hard
+git diff
+git reset HEAD --hard
+clear
+git add .idea
+git add .idea/*
+ls .idea
+ll -a
+clear
+git add README.md
+git status
+git diff
+git diff --cached
+git reset
+git checkout master
+glog
+git reset HEAD --hard
+c
+glog
+git revert df0d1cd
+glog
+git show
+git show 10b4070
+clear
+glog
+git show df0d1cd
+git show 10b4070
+cd ../pyspark-training
+clear
+glog
+git show 33ada33
+clear
+git diff
+clear
+cd repos/newfolder
+ll
+git add README.md
+git commit -m 'Sign'
+rm README.md
+git reset HEAD --hard
+clear
+glog
+git checkout -b window
+glog
+git diff
+git add -u
+git status
+git diff --cached
+git commit -m 'Add ref to dad'
+clear
+glog
+git checkout master
+git checkout -b door
+git add -u
+git status
+git diff --cached
+glog
+git commit -m 'Add new section\
+'
+clear
+glog
+git checkout window
+cat README.md
+git checkout door
+cat README.md
+clear
+git diff window
+git merge window
+clear
+cat README.md
+git status
+git commit
+clear
+glog
+git checkout master
+git merge door
+glog
+git show
+clera
+clear
+git show
+glog
+git log --oneline
+git show 10b4070
+clear
+git show
+git checkout -b left
+git add -u 
+git diff --cached
+git commit
+c
+glog
+git show
+c
+git checkout master
+git checkout -b right
+git diff
+git add -u 
+git diff --cached
+git commit -m 'Reveal my secret identity'
+clear
+glog
+git merge left
+glog
+git checkout master
+glog
+c
+glog
+./pycharm-2020.3.4/bin/pycharm.sh
+git checkout -b white
+git add -u 
+git diff --cached
+git commit -m 'Gandalf'
+glog
+git merge right
+clear
+git status
+git commit -m 'Resolve my split personality issues'
+glog
+git checkout master
+git merge white
+glog
+clear
+c
+cd repos
+mkdir second_version
+cd second_version
+git init
+git remote add backup /home/dani/repos/second_version
+glog
+clear
+git push
+git push backup
+git push --set-upstream backup master
+git status
+git checkout -b temp
+ls
+glog
+ls
+git checkout master
+ls
+glog
+git remote add origin https://github.com/danimateos/newfolder.git\
+git branch -M main\
+git push -u origin main
